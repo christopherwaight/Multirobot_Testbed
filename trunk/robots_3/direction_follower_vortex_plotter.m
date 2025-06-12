700da50d574acbd9a5776dfefefae472b292088c
christopherwaight <christopherwaight@gmail.com>
2025-06-12 13:04:25 -0700

    Adding Circular Orbits
    Circular Orbit primitive for centers

---
+++ trunk/robots_3/direction_follower_vortex_plotter.m
@@ -0,0 +1,8 @@
+figure()
+plot(cluster_pose(:,1),cluster_pose(:,2));
+hold on
+plot(robot1_pose(:,1),robot1_pose(:,2));
+plot(robot2_pose(:,1),robot2_pose(:,2));
+plot(robot3_pose(:,1),robot3_pose(:,2));
+figure()
+plot(cluster_pose(:,1),cluster_pose(:,2));
